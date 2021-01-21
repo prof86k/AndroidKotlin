d5986ae9e6e5fff924c7e9632594bffbb2feff97
= <=>
2021-01-21 02:07:12 +0000

    initial files

---
+++ app/src/main/java/com/example/kotlin_variables/song.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_variables
+/*
+ song attrs
+*
+* */
+
+var songTitle:String = "One Step at a Time"
+
+val duration:Long = 4000L
+
+val yearofRelease:String = "10/06/2008"
+
+val Recorded:String = "October 2007"
+
+val artist:String = "Jordin Sparks"
+
+var genre:String = "Pop; R&B"
+
+val album:String = "debut"
+
+val songWritter = "Robbie Nevil"
+
+val producer:String = "Jonas Jeberg"
+
